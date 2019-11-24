86508d867ba58aadb5f4d8c3e54b024309babc51
Sarah Kardache <sarah.kardache@edu.ece.fr>
2019-11-25 00:27:12 +0100

    cours 02 part2

---
+++ Cours02_02/src/server.ts
@@ -0,0 +1,26 @@
+import express = require('express')
+import { MetricsHandler } from './metrics'
+
+const app = express()
+const port: string = process.env.PORT || '8080'
+
+app.get('/', (req: any, res: any) => {
+  res.write('Hello world')
+  res.end()
+})
+
+app.listen(port, (err: Error) => {
+  if (err) {
+    throw err
+  }
+  console.log(`server is listening on port ${port}`)
+})
+
+app.get('/metrics.json', (req: any, res: any) => {
+  MetricsHandler.get((err: Error | null, result?: any) => {
+    if (err) {
+      throw err
+    }
+    res.json(result)
+  })
+})
