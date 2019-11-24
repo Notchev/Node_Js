9e8dc6623025d5c175b98aec391c65b3b0f18465
Sarah Kardache <sarah.kardache@edu.ece.fr>
2019-11-24 19:33:09 +0100

    cours 02

---
+++ COURS02/index.js
@@ -0,0 +1,30 @@
+express = require('express')
+app = express()
+metrics = require('./metrics')
+path = require('path')
+console.log(__dirname)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('port', 1340)
+
+app.set('views', __dirname + "/views")
+app.set('view engine', 'ejs');
+
+app.get(
+    '/hello/:name', 
+    (req, res) => res.render('hello.ejs', {name: req.params.name})
+  )
+  
+  app.get('/metrics.json', (req, res) => {
+    metrics.get((err, data) => {
+      if(err) throw err
+      res.status(200).json(data)
+    })
+  })
+app.listen(
+  app.get('port'), 
+  () => console.log(`server listening on ${app.get('port')}`)
+)
+
+
